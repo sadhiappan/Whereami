dbea3239d8afeb0500e3d99e085f2e74242f40c7
Sivaram Adhiappan <shivaram1190@gmail.com>
2013-05-29 23:14:49 -0500

    PaperFold Implementation
    Implemented paperfold using Cocoapod control

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// PaperFold
+#define COCOAPODS_POD_AVAILABLE_PaperFold
+#define COCOAPODS_VERSION_MAJOR_PaperFold 1
+#define COCOAPODS_VERSION_MINOR_PaperFold 1
+#define COCOAPODS_VERSION_PATCH_PaperFold 0
+
